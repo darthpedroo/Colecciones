41c64c7f1e248573a673806b3006b6e6283c56a7
darthpedroo <pedrovillarino06@gmail.com>
2024-08-28 00:56:52 -0300

    ej 5a

--- colecciones.py
+++ colecciones.py
@@ -2,19 +2,25 @@ from collections.abc import Iterable, Iterator
 
 
 class IteradorDeColeccion(Iterator):
-    def __init__(self, coleccion):
+    def __init__(self, coleccion, cant_prioridades):
         self._coleccion = coleccion
         self._indice = 0
+        self._curr_type = 1
+        self._cant_prioridades = cant_prioridades #Nose si está bien puesto aca la cantidad de prioridades porque quizas el iterador no sea el mejor lugar para ponerlo
 
     def __next__(self):
-        if self._indice < len(self._coleccion):
-            res = self._coleccion[self._indice]
-            self._indice += 1
-            if res[1] == 1:
-                return res
+        if self._curr_type <= self._cant_prioridades:  
+            if self._indice < len(self._coleccion):
+                res = self._coleccion[self._indice]
+                self._indice += 1
+                if res[1] == self._curr_type:
+                    return res
+                else:
+                    return self.__next__()
             else:
-                print("DOOPIDOPÍ: ", res)
-                # self.__next__()
+                self._curr_type += 1
+                self._indice = 0
+                return self.__next__()
         else:
             raise StopIteration
 
@@ -31,4 +37,4 @@ class ContenedorIterable(Iterable):
         self._elementos.append((elemento, tipo_prioridad))
 
     def __iter__(self):
-        return IteradorDeColeccion(self._elementos)
+        return IteradorDeColeccion(self._elementos, 9)
--- test_colecciones.py
+++ test_colecciones.py
@@ -4,22 +4,22 @@ from colecciones import ContenedorIterable
 
 class TestColecciones(unittest.TestCase):
 
-    def test_01_iniciar_contenedor_iterable(self):
-        contenedor1 = ContenedorIterable()
-        contenedor1.agregar(5, 1)
-        contenedor1.agregar(3, 1)
-        contenedor1.agregar(4, 2)
-        contenedor1.agregar(8, 1)
-        contenedor1.agregar(10, 2)
-        print(contenedor1)
+
 
     def test_02_iterar_contenedor_iterable(self):
         contenedor1 = ContenedorIterable()
-        contenedor1.agregar(5, 1)
+
+        contenedor1.agregar(5, 8)
         contenedor1.agregar(3, 1)
         contenedor1.agregar(4, 2)
         contenedor1.agregar(8, 1)
-        contenedor1.agregar(10, 2)
+        contenedor1.agregar(10, 5)
+        contenedor1.agregar(140, 8)
+        contenedor1.agregar(120, 3)
+        contenedor1.agregar(10, 3)
+        contenedor1.agregar(10, 7)
+        contenedor1.agregar(10, 5)
+        
 
         for i in contenedor1:
             print("ski", i)
